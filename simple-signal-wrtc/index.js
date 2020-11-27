d87a6041163c7fc8dfca9b75d4ca454b1f7d91e8
Vishaal <vschittur@gmail.com>
2020-11-26 22:32:04 -0500

    fixes

--- simple-signal-wrtc/index.js
+++ simple-signal-wrtc/index.js
@@ -1,26 +1,42 @@
 const signalhub = require('signalhub')
+const createSwarm = require('webrtc-swarm')
 const hub = signalhub('spaces', [
 	'http://localhost:8080'
 ])
+// new webrtc swarm with signalhub to discover + broker
+const swarm = createSwarm(hub)
 
 const Player = require('./player.js')
 const me = new Player()
 
 const peers = {}
 
-hub.subscribe('update').on('data', data =>{
-	// console.log(data)
-	if (data.color == me.color) return
-	if (!peers[data.color]) {
-		peers[data.color] = new Player(data)
+// Create WRTC Swarm and map it with Signalhub
+// peer is a simple-peer instance
+swarm.on('connect', (peer, id) => {
+	if (!peers[id]) {
+		peers[id] = new Player()
 	}
-	peers[data.color].update(data)
+	// peer.send receiver
+	peer.on('data', data => {
+		data = JSON.parse(data.toString())
+		peers[id].update(data)
+	})
+})
+
+swarm.on('disconnect', (peer, id) => {
+	if (peers[id]) {
+		peers[id].element.parentNode.removeChild(peers[id].element)
+		delete peers[id]
+	}
 })
 
 setInterval(() => {
-	// hub.broadcast('update', window.location.hash)
 	me.update()
-	hub.broadcast('update', me)
+	const meString = JSON.stringify(me)
+	swarm.peers.forEach(peer => {
+		peer.send(meString)
+	})
 }, 100)
 
 document.addEventListener('keypress', e => {
