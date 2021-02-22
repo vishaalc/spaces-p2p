9d6fbc972df85d2a7f1c5fb6bad828cbb14c2781
Vishaal <vschittur@gmail.com>
2021-02-21 23:57:57 -0500

    hide keys, remove channel stuff

--- live-chat/src/App.js
+++ live-chat/src/App.js
@@ -1,6 +1,7 @@
 import React, {
   useState,
-  useEffect
+  useEffect,
+  useMemo
 } from 'react';
 import './App.css';
 import useInput from './useInput.js';
@@ -17,34 +18,25 @@ import {
 } from '@material-ui/core';
 
 function App() {
-  // Set default channel
-  let defaultChannel = "Global";
-  let query = window.location.search.substring(1);
-  let params = query.split("&");
-  for (let i = 0; i < params.length; i++) {
-    var pair = params[i].split("=");
-    if (pair[0] === "channel" && pair[1] !== "") {
-      defaultChannel = decodeURI(pair[1]);
-    }
-  }
-
-  // Set state
-  const [channel, setChannel] = useState(defaultChannel);
+  // Set states
+  const defaultChannel = "Screening Room";
+  const [channel,] = useState(defaultChannel);
   const [messages, setMessages] = useState([]);
-  const [username, ] = useState(['user', new Date().getTime()].join('-'));
-  const tempChannel = useInput();
-  const tempMessage = useInput();
-
-  // Set up PubNub, handle eents, rerun on channel update
-  useEffect(() => {
-    console.log("Setting up PubNub");
-    const pubnub = new PubNub({
-      publishKey: "pub-c-5ab0e177-a72b-4a58-861c-0fc226fc4243",
-      subscribeKey: "sub-c-b195c3ec-7262-11eb-9641-66d8dbfc759a",
+  const [username,] = useState(['user', new Date().getTime()].join('-'));
+  const chatMessage = useInput();
+  const pubnub = useMemo(() => {
+    return new PubNub({
+      publishKey: process.env.REACT_APP_PUBLISH_KEY,
+      subscribeKey: process.env.REACT_APP_SUBSCRIBE_KEY,
       uuid: username
     });
+  }, [username])
 
-    // Add PubNub listeners
+  // Set up PubNub
+  useEffect(() => {
+    console.log("Setting up PubNub");
+
+    // Add listeners
     pubnub.addListener({
       status: (statusEvent) => {
         if (statusEvent.category === "PNConnectedCategory") {
@@ -53,7 +45,6 @@ function App() {
       },
       message: (msg) => {
         if (msg.message.text) {
-          // console.log(msg.message.text)
           let newMessages = [];
           newMessages.push({
             uuid: msg.message.uuid,
@@ -64,7 +55,7 @@ function App() {
       }
     });
 
-    // Subscribe to state channel
+    // Subscribe to channel
     pubnub.subscribe({
       channels: [channel]
     });
@@ -86,21 +77,12 @@ function App() {
     });
 
     // End
-    return function cleanup() {
-      console.log("shutting down pubnub");
+    return () => {
+      console.log("Shutting down pubnub");
       pubnub.unsubscribeAll();
       setMessages([]);
     }
-  }, [channel, username]);
-
-  // Back listener
-  useEffect(() => {
-    window.addEventListener("popstate", goBack);
-
-    return function cleanup() {
-      window.removeEventListener("popstate", goBack);
-    }
-  }, []);
+  }, [pubnub, channel, username]);
 
   // Handle inputs
   function handleKeyDown(event) {
@@ -108,69 +90,23 @@ function App() {
       if (event.key === 'Enter') {
         publishMessage();
       }
-    } else if (event.target.id === "channelInput") {
-      if (event.key === 'Enter') {
-        // Navigates to new channels
-        const newChannel = tempChannel.value.trim();
-        if (newChannel) {
-          if (channel !== newChannel) {
-            // If different channel
-            setChannel(newChannel);
-            let newURL = window.location.origin + "?channel=" + newChannel;
-            window.history.pushState(null, '', newURL);
-            tempChannel.setValue('');
-          }
-        } else {
-          // If no channel input
-          if (channel !== "Global") {
-            // If different channel
-            setChannel("Global");
-            let newURL = window.location.origin;
-            window.history.pushState(null, '', newURL);
-            tempChannel.setValue('');
-          }
-        }
-      }
     }
   }
 
   // Sending messages via PubNub
   function publishMessage() {
-    if (tempMessage.value) {
+    if (chatMessage.value) {
       let messageObject = {
-        text: tempMessage.value,
+        text: chatMessage.value,
         uuid: username
       };
-
-      const pubnub = new PubNub({
-        publishKey: "pub-c-5ab0e177-a72b-4a58-861c-0fc226fc4243",
-        subscribeKey: "sub-c-b195c3ec-7262-11eb-9641-66d8dbfc759a",
-        uuid: username
-      });
 
       pubnub.publish({
         message: messageObject,
         channel: channel
       });
 
-      tempMessage.setValue('');
-    }
-  }
-
-  function goBack() {
-    let query = window.location.search.substring(1);
-    if (!query) {
-      setChannel("Global")
-    } else {
-      let params = query.split("&");
-      for (let i = 0; i < params.length; i++) {
-        var pair = params[i].split("=");
-        //If the user input a channel then the default channel is now set
-        //If not, we still navigate to the default channel.
-        if (pair[0] === "channel" && pair[1] !== "") {
-          setChannel(decodeURI(pair[1]))
-        }
-      }
+      chatMessage.setValue('');
     }
   }
 
@@ -179,18 +115,9 @@ function App() {
     <Card >
         <CardContent>
           <div className="top">
-            <Typography variant="h4" inline >
-              PubNub React Chat
-              </Typography>
-            <Input
-              style={{width:'100px'}}
-              className="channel"
-              id="channelInput"
-              onKeyDown={handleKeyDown}
-              placeholder ={channel}
-              onChange = {tempChannel.onChange}
-              value={tempChannel.value}
-            />
+            <Typography variant="h4" inline="true">
+              Screening Room Chat
+            </Typography>
           </div>
           <div >
             <Log messages={messages}/>
@@ -201,8 +128,8 @@ function App() {
             placeholder="Enter a message"
             fullWidth={true}
             id="messageInput"
-            value={tempMessage.value}
-            onChange={tempMessage.onChange}
+            value={chatMessage.value}
+            onChange={chatMessage.onChange}
             onKeyDown={handleKeyDown}
             inputProps={{'aria-label': 'Message Field',}}
             autoFocus={true}
